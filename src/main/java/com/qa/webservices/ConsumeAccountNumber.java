5f01c251b8c7b5ace3527d8be2b3fe64cc2a7535
jtbamford <tom.bamford5@yahoo.co.uk>
2018-11-27 16:43:29 +0000

    file structure improved

--- src/main/java/com/qa/webservices/ConsumeAccountNumber.java
+++ src/main/java/com/qa/webservices/ConsumeAccountNumber.java
@@ -1,4 +1,4 @@
-package com.qa.service.webservices;
+package com.qa.webservices;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
